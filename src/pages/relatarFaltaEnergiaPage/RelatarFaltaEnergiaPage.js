351eb73d23eb68ca7770f43708761240737e0bad
luiz bernardi <luizbernardi37@gmail.com>
2025-02-21 11:14:11 -0300

    terminando modal como verificar disjuntor

--- src/pages/relatarFaltaEnergiaPage/RelatarFaltaEnergiaPage.js
+++ src/pages/relatarFaltaEnergiaPage/RelatarFaltaEnergiaPage.js
@@ -1,70 +1,46 @@
 import Box from "@mui/material/Box";
 import { useState, useEffect } from "react";
 import { Typography } from "@mui/material";
-import {FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField} from "@mui/material";
+import { FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup } from "@mui/material";
 import StepHeaderCardComponent from "../../components/stepHeaderCardComponent/stepHeaderCardComponent";
 import CardMeuImovel from "./components/CardMeuImovel";
 import CardVizinho from "./components/CardVizinho";
 import CardPoste from "./components/CardPoste";
-import CustomButton from "../../components/customButton/CustomButton";
-import InfoModal from "../../components/infoModal/InfoModal";
+import ModalSteps from "./components/modalSteps";
 
+function RelatarFaltaEnergiaPage({ steps, currentStep, nextStep }) {
+    const [instalacoes, setInstalacoes] = useState([]);
+    const [radioValue, setRadioValue] = useState("");
+    const [openModal, setOpenModal] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
 
+    const handleChangeRadio = (event) => {
+        setRadioValue(event.target.value);
+    };
 
+    const renderContent = (value) => {
+        switch (value) {
+            case 'meuimovel':
+                return <CardMeuImovel setIstalacoes={setInstalacoes} />;
+            case 'vizinhos':
+                return <CardVizinho setIstalacoes={setInstalacoes} />;
+            case 'poste':
+                return <CardPoste setIstalacoes={setInstalacoes} />;
+            default:
+                return null;
+        }
+    };
 
-
-function RelatarFaltaEnergiaPage({steps, currentStep, nextStep, prevStep}) {
-        const [instalacoes, setInstalacoes] = useState([]);
-        const [radioValue, setRadioValue] = useState("");
-        const [openModal, setOpenModal] = useState(false);
-        const [hasOpenedModal, setHasOpenedModal] = useState(false); // Estado para verificar se já abriu a modal
-        const [modalStep, setModalStep] = useState(1); // Estado para controlar os passos internos da modal
-        const [hideCloseButton, setHideCloseButton] = useState(true); // Defina o estado aqui
-
-        const handleChangeRadio = (event) => {
-            setRadioValue(event.target.value);
-        };
-
-        const renderContent = (value) => {
-            switch (value) {
-                case 'meuimovel':
-                    return <CardMeuImovel setIstalacoes={setInstalacoes}/>;
-                case 'vizinhos':
-                    return <CardVizinho setIstalacoes={setInstalacoes}/>;
-                case 'poste':
-                    return <CardPoste setInstalacoes={setInstalacoes}/>;
-                default:
-                    return null;
-            }
-        };
-
-        useEffect(() => {
-            if (radioValue === "meuimovel" && !hasOpenedModal) {
-                setOpenModal(true);
-                setHasOpenedModal(true); // Garante que a modal só abre uma vez automaticamente
-            }
-        }, [radioValue, hasOpenedModal]);
-
-        const handleNextModalStep = () => {
-            if (modalStep < 2) {
-                setModalStep(modalStep + 1);
-            } else {
-                setOpenModal(false); // Só fecha a modal quando o usuário finalizar
-            }
-        };
-
-        const handlePrevModalStep = () => {
-            if (modalStep > 1) {
-                setModalStep(modalStep - 1);
-            }
-        };
-
-        
+    useEffect(() => {
+        if (radioValue === "meuimovel" && !isChecked) {
+            setOpenModal(true); 
+        }
+    }, [radioValue, isChecked]);
 
     return (
         <Box className="falta-energia-container">
             <Box className="header-title">
-            FALTA DE ENERGIA - RELATAR FALTA DE ENERGIA
+                FALTA DE ENERGIA - RELATAR FALTA DE ENERGIA
             </Box>
             <StepHeaderCardComponent
                 steps={steps}
@@ -73,7 +49,7 @@ function RelatarFaltaEnergiaPage({steps, currentStep, nextStep, prevStep}) {
             <Box className="intro-text">
                 <h4>Para um atendimento mais assertivo, nos ajude a identificar o problema respondendo as opções abaixo</h4>
             </Box>
-            <Box sx={{mt: 3}}>
+            <Box sx={{ mt: 3 }}>
                 <Grid container spacing={5}>
                     <Grid item md={5}>
                         <Box className="falta-energia-container-relatar">
@@ -93,64 +69,13 @@ function RelatarFaltaEnergiaPage({steps, currentStep, nextStep, prevStep}) {
                 </Grid>
             </Box>
             {renderContent(radioValue)}
-
-           
-            <InfoModal
+            <ModalSteps
                 open={openModal}
-                hideCloseButton={hideCloseButton} // Aqui, `true` para esconder o botão de fechar
-                content={
-                    <Box width={600} height={550}>
-                        {modalStep === 1 && (
-                            <>
-                                <Typography >
-                                <b className="danger-text">ATENÇÃO:</b> Tome cuidado ao manusear o disjuntor, se notar alguma<br></br> anormalidade chame um profissional de sua confiança para realizar o procedimento!
-                                </Typography>
-                                <Typography sx={{ mt: 2, fontSize: "0.875rem" }}>
-                                Siga o passo a passo para verificar se seus disjuntores estão funcionando!
-                                </Typography>
-                                <Typography sx={{mt: 4}} className="blue-text" fontWeight="bold"> 
-                                    Passo 01:
-                                </Typography>
-                                <Typography sx={{mt: 2}} >
-                                Identificar e verificar disjuntores externos (do padrão) e internos (da residência)
-                                </Typography>
-                                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", mt: 4 }}>
-                                    <img src="/images/modal-steps-img-01.png" alt="Disjuntor"/>
-                                </Box>
-                                <Box sx={{ mt: 9, display: "flex", justifyContent: "space-evenly" }}>
-                                    <CustomButton label="Confirmar" onClick={handleNextModalStep} />
-                                </Box>
-                            </>
-                        )}
-
-                        {modalStep === 2 && (
-                            <>
-                               <Typography >
-                                <b className="danger-text">ATENÇÃO:</b> Tome cuidado ao manusear o disjuntor, se notar alguma<br></br> anormalidade chame um profissional de sua confiança para realizar o procedimento!
-                                </Typography>
-                                <Typography sx={{ mt: 2, fontSize: "0.875rem" }}>
-                                Siga o passo a passo para verificar se seus disjuntores estão funcionando!
-                                </Typography>
-                                <Typography sx={{mt: 4}} className="blue-text" fontWeight="bold"> 
-                                    Passo 02:
-                                </Typography>
-                                <Typography sx={{ mt: 2, fontSize: "0.875rem" }}>
-                                Desarmar as chaves (posição desligada) e armar novamente.
-                                </Typography>
-                                <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", mt: 4 }}>
-                                    <img src="/images/modal-steps-img-02.png" alt="Disjuntor" />
-                                </Box>
-                                <Box sx={{ mt: 5, display: "flex", justifyContent: "center", gap:3 }}>
-                                    <CustomButton label="Voltar" onClick={handlePrevModalStep} />
-                                    <CustomButton label="Próxima" onClick={() => { handleNextModalStep(); nextStep(); }} />
-                                </Box>
-                            </>
-                        )}
-                    </Box>
-                }
+                setOpenModal={setOpenModal} 
+                isChecked={isChecked}
+                setIsChecked={setIsChecked}
             />
         </Box>
-        
     );
 }
 
