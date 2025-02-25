34372446c3436d54bb71b9b25769e73b032a5e04
luiz bernardi <luizbernardi37@gmail.com>
2025-02-25 10:42:38 -0300

    fazendo condicional se ja existe protocolo para aquela região ou não

--- src/pages/protocoloFaltaEnergiaPage/ProtocoloFaltaEnergiaPage.js
+++ src/pages/protocoloFaltaEnergiaPage/ProtocoloFaltaEnergiaPage.js
@@ -1,7 +1,7 @@
 import Box from "@mui/material/Box";
 import {
-    Grid
-
+    Grid,
+    Typography
 } from "@mui/material";
 import * as React from "react";
 import { useState } from "react";
@@ -14,7 +14,18 @@ import CardProtocolo from "./components/CardProtocolo";
 
 function ProtocoloFaltaEnergiaPage({ steps, currentStep, prevStep }) {
 
-    const [protocolo, setProtocolo] = useState("1234567890");
+    const [protocolo, setProtocolo] = useState("123456");//mockando o protocolo
+
+    const dataHoraPrevisaoProtocolo = new Date().toLocaleString("pt-BR", {
+        timeZone: "America/Sao_Paulo",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+    }); //mochando data do protocolo ja gerado 
+
+
 
 
 
@@ -28,26 +39,56 @@ function ProtocoloFaltaEnergiaPage({ steps, currentStep, prevStep }) {
                     steps={steps}
                     currentStep={currentStep}
                 />
-                <Box>
-                    <Grid container spacing={2}>
-                        <Grid item md={12}>
-                            <Box fontWeight="bold" className="title-protocolo">
-                                Nossa equipe será acionada para resolver o problema o mais rápido possível!
-                            </Box>
-                            <Box className="container-image-protocolo" sx={{ backgroundImage: "url(/images/falta-energia-protocol-sucesso.png)", }}>
-                            </Box>
-                            <Box className="text-protocolo" marginTop={4}>
-                                Nossa equipe percorrerá a região para restabelecer a energia o mais rápido possível!
-                            </Box>
-                            <Box className="text-protocolo" marginTop={3}>
-                                Assim que solucionado o problema estaremos restabelecendo a energia no local.
-                            </Box>
-                            <CardProtocolo protocolo={protocolo}>
+                {protocolo ? (
+                    <Box>
+                        <Grid container spacing={2}>
+                            <Grid item md={12}>
+                                <Box fontWeight="bold" className="title-protocolo">
+                                    Sua região está passando por Falta de Energia já identificada!
+                                </Box>
+                                <Box className="container-image-protocolo" sx={{ backgroundImage: "url(/images/falta-energia-protocol-identificada.png)", }}>
+                                </Box>
+                                <Box className="text-protocolo" textAlign={"center"} marginTop={4}>
+                                    Não se preocupe! Nossas equipes estão trabalhando para<br></br>
+                                    resolver o problema o mais rápido possível.
+                                </Box>
+                                <Box className="text-protocolo" marginTop={3}>
+                                    Sua previsão de retorno de energia é:
+                                </Box>
+                                <Box className="text-protocolo" textAlign="center" marginTop={2}>
+                                    <Typography color="#0098d1">
+                                        {dataHoraPrevisaoProtocolo}
+                                    </Typography>
+                                </Box>
 
-                            </CardProtocolo>
+                                <CardProtocolo protocolo={protocolo}>
+
+                                </CardProtocolo>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Box>
+                    </Box>
+                ) : (
+                    <Box>
+                        <Grid container spacing={2}>
+                            <Grid item md={12}>
+                                <Box fontWeight="bold" className="title-protocolo">
+                                    Nossa equipe será acionada para resolver o problema o mais rápido possível!
+                                </Box>
+                                <Box className="container-image-protocolo" sx={{ backgroundImage: "url(/images/falta-energia-protocol-sucesso.png)", }}>
+                                </Box>
+                                <Box className="text-protocolo" marginTop={4}>
+                                    Nossa equipe percorrerá a região para restabelecer a energia o mais rápido possível!
+                                </Box>
+                                <Box className="text-protocolo" marginTop={3}>
+                                    Assim que solucionado o problema estaremos restabelecendo a energia no local.
+                                </Box>
+                                <CardProtocolo protocolo={protocolo}>
+
+                                </CardProtocolo>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                )}
                 <Box className="footer-form">
                     <CustomButton
                         label="Voltar"
